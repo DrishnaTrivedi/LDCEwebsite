4da308af479f5a8cda6cd87cd753fa3d49d22e5b
Punja007 <kuchhadiyapunja007@gmail.com>
2024-05-16 19:00:39 +0530

    added the count up animation and added 'countup background' image by punja and vishal

--- script.js
+++ script.js
@@ -36,3 +36,44 @@ function toggleMenu(){
 function toggle(){
   menulist.style.maxHeight="0px";
 }
+
+  // count up animation code below
+  // Create a new Intersection Observer
+  const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        // Check if the target element is intersecting with the viewport
+        if (entry.isIntersecting) {
+            // Perform actions when the target element becomes visible
+            const counters = document.querySelectorAll('.count');
+            const speed = 200; // Adjust speed as needed
+
+            counters.forEach(counter => {
+                const target = parseInt(counter.getAttribute('data-count'));
+                let count = 0;
+
+                const updateCount = () => {
+                    const increment = target / speed;
+
+                    count += increment;
+                    if (count < target) {
+                        counter.textContent = Math.ceil(count);
+                        setTimeout(updateCount, 10);
+                    } else {
+                        counter.textContent = target;
+                    }   
+                };
+
+                updateCount();
+            });
+            // Unobserve the target element to prevent multiple executions
+            observer.unobserve(entry.target);
+        }
+    });
+});
+
+// Select the target element you want to observe
+const targetElement = document.querySelector('.banner-card');
+
+// Start observing the target element
+observer.observe(targetElement);
+
